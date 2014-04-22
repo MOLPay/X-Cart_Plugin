4240a547acbbd7cfab38b410b3db30a10adc7269
azwan <azwan@molpay.com>
2014-04-22 11:32:25 +0800

    fix
    fix

--- setup_molpay.php
+++ setup_molpay.php
@@ -1,50 +1,37 @@
 <?php
-/*****************************************************************************\
-+-----------------------------------------------------------------------------+
-| MOLPay - http://www.molpay.com			      						|
-| April 2014		                                                      			|
-| All rights reserved.                                                        |
-+-----------------------------------------------------------------------------+
-\*****************************************************************************/
+    /*****************************************************************************\
+    +-----------------------------------------------------------------------------+
+    | MOLPay - http://www.molpay.com                                              |
+    | April 2014		                                                  |
+    | All rights reserved.                                                        |
+    +-----------------------------------------------------------------------------+
+    \*****************************************************************************/
 
+    require "./top.inc.php";
+    require "./init.php";
 
-require "./top.inc.php";
-require "./init.php";
+    global $sql_tbl;
 
+    $check = "SELECT module_name FROM $sql_tbl[ccprocessors]"; 
+    $res   = func_query($check); 
+    $size     = sizeof($res);
+    $flag = 0; 
 
-global $sql_tbl;
+    for ($i=0; $i<$size; $i++) {
 
+      if( $res[$i]['module_name']=="MOLPay Online Payment Gateway" ) { $flag = 1; }
 
-$check = "SELECT module_name FROM $sql_tbl[ccprocessors]"; 
+    }
+    $insert="INSERT INTO xcart_ccprocessors SET module_name='MOLPay Online Payment Gateway', type='C', processor='cc_molpay.php', template='cc_molpay.tpl', disable_ccinfo='Y', background='N', testmode='N', paymentid=0";
 
-$res   = func_query($check); 
-
-$size     = sizeof($res);
-
-$flag = 0; 
-
-for ($i=0; $i<$size; $i++) {
-
-  if( $res[$i]['module_name']=="MOLPay Online Payment Gateway" ) { $flag = 1; }
-  
-}
-$insert="INSERT INTO xcart_ccprocessors SET module_name='MOLPay Online Payment Gateway', type='C', processor='cc_molpay.php', template='cc_molpay.tpl', disable_ccinfo='Y', background='N', testmode='N', paymentid=0";
-
-if ( $flag == 0 ) {
-  mysql_query($insert);  
-  echo "<h3 style='font-family:Arial; font-size:10pt; text-align:center; padding:40px;'>
-         MOLPay Payment Module successfully install in your shopping cart. <br><br>Please configure this payment module on Admin site.
-       </h3>";  
-}
-
-
-else { 
-  echo "<h3 style='font-family:Arial; font-size:10pt; text-align:center; padding:40px; color:red;'>
-         MOLPay Payment Module already exist. <br><br>Please configure this payment module on Admin site.
-       </h3>";  
-}
-
-
-        
-
+    if ( $flag == 0 ) {
+      mysql_query($insert);  
+      echo "<h3 style='font-family:Arial; font-size:10pt; text-align:center; padding:40px;'>
+             MOLPay Payment Module successfully install in your shopping cart. <br><br>Please configure this payment module on Admin site.
+           </h3>";  
+    } else { 
+      echo "<h3 style='font-family:Arial; font-size:10pt; text-align:center; padding:40px; color:red;'>
+             MOLPay Payment Module already exist. <br><br>Please configure this payment module on Admin site.
+           </h3>";  
+    }       
 ?>
--- upload/cc_molpay.tpl
+++ upload/cc_molpay.tpl
@@ -1,7 +1,7 @@
 {* $Id: cc_echo.tpl,v 1.6.2.3 2007/02/15 06:34:49 svowl Exp $ *}
 <h3>MOLPay Online Payment Gateway</h3>
 {$lng.txt_cc_configure_top_text}
-<p />
+<p/>
 {capture name=dialog}
 <br>
 1.  Put your MOLPay Merchant ID into 'MOLPay Merchant ID' field below. <br><br>
@@ -9,25 +9,37 @@
 3. Please provide MOLPay Return URL to verify order made using MOLPay Online Payment Gateway. i.e: <b>http://www.storedomain.com/payment/cc_molpay_process.php</b><br>Replace <u>www.storedomain.com</u> with your own xcart URL.
 <br>
 <form action="cc_processing.php?cc_processor={$smarty.get.cc_processor|escape:"url"}" method="post">
-<center>
-<table cellspacing="10">
-<tr>
-<td>MOLPay Merchant ID:</td>
-<td><input type="text" name="param01" size="32" value="{$module_data.param01|escape}" />
-</td>
-</tr>
-<tr>
-<td>MOLPay Verify Key:</td>
-<td><input type="text" name="param02" size="32" value="{$module_data.param02|escape}" /></td>
-</tr>
-<tr>
-<td>MOLPay Return URL:</td>
-<td><input type="text" name="param03" size="32" value="{$module_data.param03|escape}" /></td>
-</tr>
-</table>
-<p />
-<input type="submit" value="{$lng.lbl_update|strip_tags:false|escape}" />
+    <center>
+    <table cellspacing="10">
+        <tr>
+            <td>
+                MOLPay Merchant ID:
+            </td>
+            <td>
+                <input type="text" name="param01" size="32" value="{$module_data.param01|escape}" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                MOLPay Verify Key:
+            </td>
+            <td>
+                <input type="text" name="param02" size="32" value="{$module_data.param02|escape}" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                MOLPay Return URL:
+            </td>
+            <td>
+                <input type="text" name="param03" size="32" value="{$module_data.param03|escape}" />
+            </td>
+        </tr>
+    </table>
+    <p/>
+    <input type="submit" value="{$lng.lbl_update|strip_tags:false|escape}" />
+    </center>
 </form>
-</center>
+    
 {/capture}
 {include file="dialog.tpl" title=$lng.lbl_cc_settings content=$smarty.capture.dialog extra='width="100%"'}
